ca1f90acfde95afb26bd1694839efeca9610e9d7
wangfangcao277 <wangfangcao277@gmail.com.cn>
2019-07-02 18:14:02 +0800

    gka合成图片

---
+++ gka-a-canvas-s-sp-1/gka-data.js
@@ -0,0 +1,146 @@
+var data = {
+    "sourceW": 400,
+    "sourceH": 300,
+    "w": 1787,
+    "h": 244,
+    "y": 0,
+    "file": "sprites.png",
+    "frames": [
+        {
+            "width": 237,
+            "height": 237,
+            "offX": 95,
+            "offY": 42,
+            "x": 0
+        },
+        {
+            "width": 161,
+            "height": 243,
+            "offX": 148,
+            "offY": 36,
+            "x": 237
+        },
+        {
+            "width": 20,
+            "height": 15,
+            "offX": 304,
+            "offY": 265,
+            "x": 398
+        },
+        {
+            "width": 111,
+            "height": 235,
+            "offX": 156,
+            "offY": 43,
+            "x": 418
+        },
+        {
+            "width": 14,
+            "height": 15,
+            "offX": 280,
+            "offY": 265,
+            "x": 529
+        },
+        {
+            "width": 27,
+            "height": 15,
+            "offX": 297,
+            "offY": 265,
+            "x": 543
+        },
+        {
+            "width": 186,
+            "height": 205,
+            "offX": 137,
+            "offY": 76,
+            "x": 570
+        },
+        {
+            "width": 225,
+            "height": 163,
+            "offX": 92,
+            "offY": 121,
+            "x": 756
+        },
+        {
+            "width": 7,
+            "height": 15,
+            "offX": 317,
+            "offY": 265,
+            "x": 981
+        },
+        {
+            "width": 148,
+            "height": 224,
+            "offX": 113,
+            "offY": 59,
+            "x": 988
+        },
+        {
+            "width": 14,
+            "height": 15,
+            "offX": 280,
+            "offY": 265,
+            "x": 1136
+        },
+        {
+            "width": 27,
+            "height": 15,
+            "offX": 297,
+            "offY": 265,
+            "x": 1150
+        },
+        {
+            "width": 111,
+            "height": 237,
+            "offX": 153,
+            "offY": 46,
+            "x": 1177
+        },
+        {
+            "width": 14,
+            "height": 15,
+            "offX": 280,
+            "offY": 265,
+            "x": 1288
+        },
+        {
+            "width": 27,
+            "height": 15,
+            "offX": 297,
+            "offY": 265,
+            "x": 1302
+        },
+        {
+            "width": 172,
+            "height": 244,
+            "offX": 94,
+            "offY": 41,
+            "x": 1329
+        },
+        {
+            "width": 14,
+            "height": 15,
+            "offX": 280,
+            "offY": 265,
+            "x": 1501
+        },
+        {
+            "width": 27,
+            "height": 15,
+            "offX": 297,
+            "offY": 265,
+            "x": 1515
+        },
+        {
+            "width": 245,
+            "height": 231,
+            "offX": 79,
+            "offY": 52,
+            "x": 1542
+        }
+    ],
+    "animations": {
+        "a": [[0],[1,2],[3,4,5],[6],[7,8],[9,10,11],[12,13,14],[15,16,17],[18]]
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+https://gka.js.org/#/?id=gka
+```bash
+gka a -s -m -t --bgcolor'#ff0000' --split --minirate'min-max'
+```
+生成了 gka-a-css 文件夹下的 html
